51c51a721286519281f18f8a74375171b27e5652
DavidFranklin <davidfranklin777@gmail.com>
2022-05-07 15:12:32 +0200

    Fixed code
    Fixed constructors:
    You shouldnt delete pointers in constructors, it is not possible to call
    constructors again after the object is already created.
    So removed delete operations -> they will crash when they try to delete
    a pointer that doesn't exist.
    
    Used char pointer:
    Created an integer variable to track the len of the char pointer/array.

--- main.cpp
+++ main.cpp
@@ -8,38 +8,55 @@ using namespace std;
 class ruleOfFive
 {
 public:
-    int var;
-    int *buffer;
 
-    // Assignment operator overloading
-    // This helps us perform obj1 = obj2 effectively
-    // obj1,obj2 are the objects of ruleOfFive
-    ruleOfFive& operator=(const ruleOfFive& obj)
+    // Default constructor
+    // initialize everything to 0 or nullptr
+    ruleOfFive()
+        : var(0)
+        , len(0)
+        , buffer(nullptr)
     {
-        delete buffer;
-        if(this != &obj)
-        {
-            var = obj.var;
-            copy(obj.buffer, obj.buffer+sizeof(obj.buffer), buffer);
-        }
-        return *this;
+        cout << "Default constructor " << endl;
+    }
+
+    // User defined constructor
+    // Use this to simply initialize the object
+    ruleOfFive(int _var, int _len, char* _buffer)
+        : var(_var)
+        , len(_len)
+    {
+        buffer = new char(len);
+        strncpy(buffer, _buffer, len);
     }
 
     // Copy constructor
     // This helps us perform obj1(obj2) effectively
     ruleOfFive(const ruleOfFive& obj)
     {
-        delete buffer;
-        var = obj.var;
-        copy(obj.buffer, obj.buffer+sizeof(obj.buffer), buffer);
+        if(this != &obj)
+        {
+            var = obj.var;
+            len = obj.len;
+            buffer = new char(len);
+            strncpy(buffer, obj.buffer, len);
+        }
     }
 
-    // Default constructor
-    ruleOfFive()
-        : var(0)
-        , buffer(new int)
+    // Assignment operator overloading
+    // This helps us perform obj1 = obj2 effectively
+    // obj1,obj2 are the objects of ruleOfFive
+    ruleOfFive& operator=(const ruleOfFive& obj)
     {
-        cout << "Constructor " << endl;
+        if(this != &obj)
+        {
+            if(buffer != nullptr)
+                delete buffer;
+            var = obj.var;
+            len = obj.len;
+            buffer = new char(len);
+            strncpy(buffer, obj.buffer, len);
+        }
+        return *this;
     }
 
     // Move assignment operator overloading
@@ -48,11 +65,20 @@ public:
     {
         // If we do not delete buffer here, it will be overwritten by a new memory address
         // That causes the previous buffer to be incapable of accessing, thus making it impossible to delete
-        delete buffer;
-        swap(var, obj.var);
-        swap(buffer, obj.buffer);
-        obj.buffer = NULL;
-        obj.var = 0;
+        if(this != &obj)
+        {
+            if (buffer != nullptr)
+                delete buffer;
+            var = obj.var;
+            len = obj.len;
+            buffer = new char(len);
+            strncpy(buffer, obj.buffer, len);
+            delete obj.buffer;
+            obj.buffer = nullptr;
+            obj.var = 0;
+            obj.len = 0;
+        }
+
         return *this;
     }
 
@@ -60,37 +86,99 @@ public:
     // This helps us perform obj1(move(obj2))
     ruleOfFive(ruleOfFive &&obj)
     {
-        // If we do not delete buffer here, it will be overwritten by a new memory address
-        // That causes the previous buffer to be incapable of accessing, thus making it impossible to delete
-        delete buffer;
-        swap(var, obj.var);
-        swap(buffer, obj.buffer);
-        obj.buffer = NULL;
+
+        var = obj.var;
+        len = obj.len;
+        buffer = new char(len);
+        strncpy(buffer, obj.buffer, len);
+        obj.buffer = nullptr;
         obj.var = 0;
+        obj.len = 0;
     }
 
 
     // Destructor
     ~ruleOfFive()
     {
-        delete buffer;
-        buffer = NULL;
+        if(this->buffer!= nullptr)
+        {
+            delete buffer;
+            buffer = nullptr;
+        }
+
         cout << "Destructor " << endl;
     }
+
+    int getVar()
+    {
+        return var;
+    }
+
+    int getLen()
+    {
+        return len;
+    }
+
+    char* getBuffer()
+    {
+        return buffer;
+    }
+
+private:
+    int var;
+    int len;
+    char *buffer;
 
 };
 
 int main()
 {
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // Testing
-    ruleOfFive obj1, obj2;
-    obj1.var = 1;
-    obj1.buffer[0] = 55;
-    cout << "initial " << obj1.buffer[0] << endl;
-    obj2 = move(obj1);
+    char s[] = "AttackOnTitan";
+    ruleOfFive obj1(1,sizeof(s), s);
+    ruleOfFive obj2(obj1);            // testing copy constructor
+    ruleOfFive obj3;
+    obj3 = obj1;                      // testing assignment operator
+    ruleOfFive obj4(move(obj2));      // testing move constructor
+    ruleOfFive obj5 = move(obj1);     // testing move assignment operator
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    //cout << "obj1 var = " << obj1.var << " buf " << obj1.buffer[0] << endl;
-    cout << "obj2 var = " << obj2.var << " buf " << obj2.buffer[0] << endl;
+    cout << "obj2 = ";
+    for(int i=0; i<obj2.getLen(); i++)
+    {
+        cout << *(obj2.getBuffer()+i);
+    }
+
+    cout << endl;
+
+    cout << "obj3 = ";
+    for(int i=0; i<obj3.getLen(); i++)
+    {
+        cout << *(obj3.getBuffer()+i);
+    }
+
+    cout << endl;
+
+    cout << "obj4 = ";
+    for(int i=0; i<obj4.getLen(); i++)
+    {
+        cout << *(obj4.getBuffer()+i);
+    }
+
+    cout << endl;
+
+    cout << "obj5 = ";
+    for(int i=0; i<obj5.getLen(); i++)
+    {
+        cout << *(obj5.getBuffer()+i);
+    }
+
+    cout << endl;
+
+
 
     return 0;
 }
